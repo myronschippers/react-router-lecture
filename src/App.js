1fc864dcf0849170a95287e1c03dacb579b7f705
Myron <dubtwee@gmail.com>
2019-05-12 22:04:13 -0500

    importing our new react-router-dom dependency into our application

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
 import Animals from './Components/Animals/Animals.js'
@@ -9,7 +10,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <Home />
+        <Plants />
+        <Animals />
       </div>
     );
   }
