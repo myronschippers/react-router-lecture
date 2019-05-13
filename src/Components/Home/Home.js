b9f053fcdfa679cba83f04e1c8578fa496b9ce26
Myron <dubtwee@gmail.com>
2019-05-13 00:03:02 -0500

    importing the newly created AnimalButton component into the Home page component and remove the content it's replacing

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,13 @@
 import React, {Component} from 'react';
+import AnimalButton from '../AnimalButton/AnimalButton';
 
 class Home extends Component {
-  handleClick = (event) => {
-    alert('You are headed to animals.');
-    // CHANG LOCATION???
-    this.props.history.push('/animals');
-  }
-
   render() {
     console.log(this.props);
     return (
       <div>
         <h1>HOME</h1>
-        <button onClick={this.handleClick}>GO TO ANIMALS</button>
+        <AnimalButton />
       </div>
     )
   }
