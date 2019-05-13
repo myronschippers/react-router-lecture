472c10be0603c4d7d15663f48b0f12e76d02500f
Myron <dubtwee@gmail.com>
2019-05-12 23:48:39 -0500

    showing the props that are passed to the components that we registered with our Route components and then demonstrate how we can push a new location to this.props.history property

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,13 +4,15 @@ class Home extends Component {
   handleClick = (event) => {
     alert('You are headed to animals.');
     // CHANG LOCATION???
+    this.props.history.push('/animals');
   }
 
   render() {
+    console.log(this.props);
     return (
       <div>
         <h1>HOME</h1>
-        <button onClick={this.handleClick}></button>
+        <button onClick={this.handleClick}>Click to Change Location</button>
       </div>
     )
   }
