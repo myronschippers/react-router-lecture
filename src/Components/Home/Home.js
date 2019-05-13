46dac1d6831f213ae0e67a5af8be5a741e6769e9
Myron <dubtwee@gmail.com>
2019-05-12 23:39:32 -0500

    creating a new button on the Home page component content to demonstrate how to leverage Link to navigation to other pages

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
+  handleClick = (event) => {
+    alert('You are headed to animals.');
+    // CHANG LOCATION???
+  }
+
   render() {
     return (
       <div>
         <h1>HOME</h1>
+        <button onClick={this.handleClick}></button>
       </div>
     )
   }
